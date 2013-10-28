064319513b1cefeca60e0d61b09d63373ba8ae92
LukeStorry <luke.storry@gmail.com>
2013-10-28 14:06:51 +0000

    Update PrimeChecker.pas
    optimized the limits for the first loop

--- PrimeChecker/PrimeChecker.pas
+++ PrimeChecker/PrimeChecker.pas
@@ -9,7 +9,7 @@ begin
   Writeln('This Program checks whether a number is prime.');
   Write('Please enter a number: '); Readln(num);
   isPrime:=True; //let's be optimistic
-  for i:=2 to Round(Sqrt(num+1)) do
+  for i:=2 to Trunc(Sqrt(num)+1) do
         if num MOD i = 0 then isPrime:=False;
   if isPrime = True
   then Writeln('That is a Prime.')
